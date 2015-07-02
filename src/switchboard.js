bf7291f57a01b87122b4dc55de86a7ebe42930ff
Jason <jason@jasonpecor.com>
2015-07-01 22:59:07 -0400

    Incremented version number to 1.0.1

--- src/switchboard.js
+++ src/switchboard.js
@@ -28,13 +28,44 @@ xtag.mixins.switchboard = (function () {
 	
 	var api = {
 		patch: function (element, events) {
-			// prepare certain events to be patched into the switchboard for this element
-			if (typeof events === 'string')
-				element._patches.push(events);
-			else if (events instanceof Array)
-				element._patches = element._patches.concat(events);
-			else
-				throw new TypeError("Cannot patch event: Must be string or array of strings");
+			
+			if (typeof element === 'object') {
+				
+				if (element instanceof HTMLElement) {
+					// prepare certain events to be patched into the switchboard for this element
+					
+					if (!element._patches)
+						Object.defineProperty(element, '_patches', {
+							value: [],
+							enumerable: false
+						});
+					
+					if (typeof events === 'string')
+						element._patches.push(events);
+					else if (events instanceof Array)
+						element._patches = element._patches.concat(events);
+					else
+						throw new TypeError("Cannot patch event: Must be string or array of strings");
+					
+					return element;						
+				}
+				
+				// Extend non-HTMLElement object, if not already extended
+				
+				if (!element._patches)
+					Object.defineProperties(element, connectionMixin);
+				
+				// Verify valid events to patch
+				
+				if (typeof events === 'string')
+					element._patches.push(events);
+				else if (events instanceof Array)
+					element._patches = element._patches.concat(events);
+				else
+					throw new TypeError("Cannot patch event: Must be string or array of strings");
+				
+				return element;					
+			}
 		},
 		connect: function (element) {
 			// connect the element to the switchboard
@@ -90,12 +121,82 @@ xtag.mixins.switchboard = (function () {
 		},
 		get connections() { // TODO: REMOVE THIS!!!
 			return connections;
+		},
+		showConnections: function () {
+			if (!console.table) return;
+			
+			var table = [['Event Name', 'Element'],[]];
+			
+			for (var i in connections) {
+				if (!connections.hasOwnProperty(i)) continue;
+				
+				var p = connections[i],
+					n = p.length;
+				
+				for (var x = 0; x < n; x++)
+					table.push([i, '<' + p[x].tagName.toLowerCase() + '>']);
+			}
+			
+			console.table(table);
 		}
 	};
 	
 	// Make Switchboard API globally available
 	
 	xtag.switchboard = api;
+	
+	// Define Connection mixin for non-HTMLElements
+	
+	var connectionMixin = {
+		_listeners: {
+			value: {},
+			enumerable: false
+		},
+		_patches: {
+			value: [],
+			enumerable: false
+		},
+		addEventListener: {
+			value: function (evt, cb, capture) {
+				if (!this._listeners[evt])
+					this._listeners[evt] = [];
+				
+				this._listeners[evt].push(cb);
+			}
+		},
+		removeEventListener: {
+			value: function (evt, cb, capture) {
+				if (!this.listeners[evt]) return;
+				
+				if (!evt && !cb) {
+					this.listeners = {};
+					
+				} else if (!cb) {
+					this.listeners[evt] = [];
+					
+				} else {
+					var i = this.listeners[evt].indexOf(cb);
+					if (i === -1) return;
+					this.listeners[evt].splice(i,1);
+				}
+			}
+		},
+		dispatchEvent: {
+			value: function (event) {
+				var t = event.type;
+				
+				if (!this._listeners[t]) return;
+				
+				var n = this._listeners[t].length;
+				
+				for (var i = 0; i < n; i++)
+					this._listeners[t][i].call(this, event);
+			}
+		},
+		tagName: {
+			value: '__OBJECT__'
+		}
+	};
 
 	// Define switchboard mixin for x-tag
 	
